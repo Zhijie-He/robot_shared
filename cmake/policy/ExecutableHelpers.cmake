922926a9d56a0fa9424048ace72225b30c76b47f
Zhijie-He <zhijiehe97@gmail.com>
2025-12-22 11:39:10 +0800

    add exectable file path for add_policy_exec func

--- cmake/policy/ExecutableHelpers.cmake
+++ cmake/policy/ExecutableHelpers.cmake
@@ -1,4 +1,4 @@
-# robot_shared/cmake/policy/ExecutableSetup.cmake
+# robot_shared/cmake/policy/ExecutableHelpers.cmake
 include_guard(GLOBAL)
 include(${CMAKE_CURRENT_LIST_DIR}/../core/ColorMessage.cmake)
 
@@ -7,10 +7,19 @@ function(add_policy_exec name)
         message(FATAL_ERROR "SRC_FILES not defined before calling add_policy_exec(${name})")
     endif()
 
-    add_executable(${name} exec/${name}.cpp ${SRC_FILES})
+    # ---- optional exec path (minimal change) ----
+    # usage:
+    #   add_policy_exec(foo)          -> exec/foo.cpp
+    #   add_policy_exec(foo tools)    -> tools/foo.cpp
+    if(ARGC GREATER 1)
+        set(exec_path "${ARGV1}")
+    else()
+        set(exec_path "exec")
+    endif()
+
+    add_executable(${name} ${exec_path}/${name}.cpp ${SRC_FILES})
     target_link_libraries(${name} PRIVATE ${COMMON_LIBS})
 
-    color_message(STATUS cyan "[INFO] Added executable target: ${name}")
+    color_message(STATUS cyan "[INFO] Added executable target: ${name} (path=${exec_path})")
 endfunction()
-
 
