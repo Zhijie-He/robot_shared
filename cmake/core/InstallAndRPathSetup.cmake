62046f1c0d18c23a275dfb8da3eb68bba64d00d2
Zhijie-He <zhijiehe97@gmail.com>
2026-01-15 21:55:37 +0800

    fix install package library finding error

--- cmake/core/InstallAndRPathSetup.cmake
+++ cmake/core/InstallAndRPathSetup.cmake
@@ -6,10 +6,15 @@ include(${CMAKE_CURRENT_LIST_DIR}/ColorMessage.cmake)
 include(GNUInstallDirs)
 color_message(STATUS cyan "[INFO] GNUInstallDirs loaded")
 
+# 禁用 new dtags → 用 RPATH
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS} -Wl,--disable-new-dtags"
+)
+
 # --- RPATH 设置 ---
 set(CMAKE_SKIP_BUILD_RPATH FALSE)
 set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
 set(CMAKE_INSTALL_RPATH "$ORIGIN/../lib")
 
 color_message(STATUS cyan "[INFO] RPATH configured to $ORIGIN/../lib")
